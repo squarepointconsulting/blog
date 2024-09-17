d2219b3e091aa23106b51e54e2199639f1ae02f1
Christopher Eckert <christopher.k.eckert@gmail.com>
2024-09-17 15:03:30 -0700

    roughed in Firebase auth with Google

---
+++ middleware/auth.global.ts
@@ -0,0 +1,12 @@
+export default defineNuxtRouteMiddleware(async (to, from) => {
+    const user = await getCurrentUser()
+
+    if (user && to.name === 'login') {
+        return navigateTo('/')
+    }
+
+    if (!user && to.name !== 'login') {
+        return navigateTo('/login')
+    }
+
+})
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,24 @@
 import { defineNuxtConfig } from 'nuxt/config'
+require("dotenv").config()
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   compatibilityDate: '2024-09-15',
-  modules: ['@nuxt/ui'],
+  ssr: false,
+  devtools: {enabled: true},
+  modules: ['@nuxt/ui', 'nuxt-vuefire'],
+  vuefire: {
+    auth: {
+      enabled: true,
+    },
+    config: {
+      apiKey: process.env.NUXT_FIREBASE_apiKey, 
+      authDomain: process.env.NUXT_FIREBASE_authDomain,
+      projectId: process.env.NUXT_FIREBASE_projectId,
+      storageBucket: process.env.NUXT_FIREBASE_storageBucket,
+      messagingSenderId: process.env.NUXT_FIREBASE_messagingSenderId,
+      appId: process.env.NUXT_FIREBASE_appId,
+      measurementId: process.env.NUXT_FIREBASE_measurementId,
+    },
+  }
 })
