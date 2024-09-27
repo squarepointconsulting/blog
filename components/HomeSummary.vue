ac481f7db75c1e7991abe728767d7d7ea49a104d
Christopher Eckert <christopher.k.eckert@gmail.com>
2024-09-27 12:34:19 -0700

    wrapping up update and delete

--- components/HomeSummary.vue
+++ components/HomeSummary.vue
@@ -1,67 +1,170 @@
 <script setup>
 import { onMounted, ref } from 'vue'
-import { getDoc, doc } from 'firebase/firestore'
+import { getDoc, doc, updateDoc, deleteDoc } from 'firebase/firestore'
 import GaugeChart from '~/components/GaugeChart.vue'
 
 const { $db } = useNuxtApp();
-const home = ref()
+const homeSource = ref()
+const router = useRouter();
+const isEditing = ref(false);
+
+const editHome = ref({
+    address: {
+        city: "",
+        state: "",
+        street1: "",
+        street2: "",
+        zip: "",
+    },
+    currentAppraisedValue: 0,
+    geoip: '',
+    villaFactScore: 0,
+    imageUrl: false,
+    ownerId: '',
+    updated_at: null,
+})
+
+// When homeSource is updated
+watch(homeSource, (homeSource) => {
+    // Update the editHome with the appropriate values
+    editHome.value = {
+        ...homeSource,
+    }
+})
+
 const props = defineProps({
-  homeId: {
-    type: Object,
-    required: true,
-  },
+    homeId: {
+        type: Object,
+        required: true,
+    },
 });
+
+async function updateHome() {
+    const docRef = doc($db, "properties", props.homeId);
+    await updateDoc(docRef, {
+        ...editHome.value,
+    })
+    isEditing.value = false
+}
+
+async function deleteHome() {
+    await deleteDoc(doc($db, "properties", props.homeId))
+    router.push('/profile');
+}
 
 onMounted(() => {
     const docRef = doc($db, "properties", props.homeId);
-    getDoc(docRef).then ((docSnap) => {
+    getDoc(docRef).then((docSnap) => {
         if (docSnap.exists()) {
-            home.value = docSnap.data()
+            homeSource.value = docSnap.data()
         } else {
             console.error("No such document!");
         }
-    })    
+    })
 })
 </script>
 <template>
-    <article v-if="home"  class="p-4 bg-white shadow-md rounded-md">
-    <div class="flex w-full">
-        <!-- Inner Div 1 -->
-        <div class="flex-1 flex flex-col justify-center items-center">
-        <div class="w-full flex justify-center items-center">
-            <p><img class="w-24 h-24 rounded-full object-fill" style="text-align: center;" :src="home.imageUrl"
-                alt="My Biggest Asset" /></p>
+    <article v-if="homeSource && !isEditing" class="p-4 bg-white shadow-md rounded-md">
+        <div class="flex w-full">
+            <!-- Inner Div 1 -->
+            <div class="flex-1 flex flex-col justify-center items-center">
+                <div class="w-full flex justify-center items-center">
+                    <img class="w-24 h-24 rounded-full object-fill" style="text-align: center;"
+                        :src="homeSource.imageUrl" alt="My Biggest Asset" />
+                </div>
+                <!-- Row 2 -->
+                <div class="w-full flex justify-center items-center p-1">
+                    <p class="">{{ homeSource.address.street1 }}</p>
+                </div>
+            </div>
+
+            <!-- Inner Div 2 -->
+            <div class="flex-1 flex flex-col justify-center items-center">
+                <!-- Row 1 -->
+                <div class="w-full flex justify-center items-center">
+                    <div class="chart-container">
+                        <GaugeChart />
+                    </div>
+                </div>
+                <!-- Row 2 -->
+                <div class="w-full flex justify-center items-center">
+                    <p class="font-bold text-center">{{ homeSource.villafactScore }}</p>
+                </div>
+            </div>
+            <!-- Edit Button -->
+            <div class="relative top-2 right-2">
+                <UButton @click="isEditing = true" icon="i-heroicons-pencil-square" class="focus:outline-none">
+                </UButton>
+            </div>
+
         </div>
-        <!-- Row 2 -->
-        <div class="w-full flex justify-center items-center p-1">
-            <p class="">{{ home.address.street1 }}</p>
-        </div>
+    </article>
+    <transition name="fade">
+        <article v-if="homeSource && isEditing" class="p-4 bg-white shadow-md rounded-md relative">
+            <div class="flex flex-col space-y-2 border">
+                <UInput v-model="homeSource.address.street1" type="text" placeholder="Street 1"
+                    class="p-2 border-gray-300 rounded-md" />
+                <UInput v-model="homeSource.address.street2" type="text" placeholder="Street 2"
+                    class="p-2  border-gray-300 rounded-md" />
+                <UInput v-model="homeSource.address.city" type="text" placeholder="City"
+                    class="p-2 border-gray-300 rounded-md" />
+                <UInput v-model="homeSource.address.state" type="text" placeholder="State"
+                    class="p-2 border-gray-300 rounded-md" />
+                <UInput v-model="homeSource.address.zip" type="text" placeholder="Zip Code"
+                    class="p-2 border-gray-300 rounded-md" />
+            </div>
+            <div class="absolute top-2 right-2">
+                <UButton @click="isEditing = false" icon="i-heroicons-x-circle"
+                            class="focus:outline-none">
+                </UButton>
+            </div>
+            <div class="flex items-center space-x-4">
+                <!-- Right slot for icons -->
+                <slot name="right-icons">
+                    <span>
+                        <UButton @click="updateHome" icon="i-heroicons-check-circle"
+                            class="" />
+                    </span>
+                    <span>
+                        <UButton @click="deleteHome" label="Delete Home"
+                            class=" bg-red-700 " />
+                    </span>
+                </slot>
+            </div>
+
+            <!-- Hidden File Input -->
+            <input ref="fileInput" type="file" class="hidden" @change="handleFileChange" />
+        </article>
+    </transition>
+    <article v-if="!homeSource" class="p-4 bg-white shadow-md rounded-md">
+
+        <div class="flex items-center space-x-4">
+            <USkeleton class="h-12 w-12" :ui="{ rounded: 'rounded-full' }" />
+            <div class="space-y-2">
+                <USkeleton class="h-4 w-[250px]" />
+                <USkeleton class="h-4 w-[200px]" />
+            </div>
         </div>
 
-        <!-- Inner Div 2 -->
-        <div class="flex-1 flex flex-col justify-center items-center">
-        <!-- Row 1 -->
-        <div class="w-full flex justify-center items-center">
-            <div class="chart-container">
-            <GaugeChart />
-            </div>
-        </div>
-        <!-- Row 2 -->
-        <div class="w-full flex justify-center items-center">
-            <p class="font-bold text-center">{{ home.villafactScore }}</p>
-        </div>
-        </div>
-    </div>
-    </article>
-    <article v-else  class="p-4 bg-white shadow-md rounded-md">
-        <div class="flex w-full">
-            <p>Loading...</p>
-            </div>
+
     </article>
 </template>
-  
+
 <style scoped>
 .chart-container {
-  height: 100px;
+    height: 100px;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 0.5s ease;
+}
+
+.fade-enter,
+.fade-leave-to
+
+/* .fade-leave-active in <2.1.8 */
+    {
+    opacity: 0;
 }
 </style>
