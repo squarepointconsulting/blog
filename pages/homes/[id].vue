0fd3e3a68c53c87b9e5b48c2dc2e4c92878e9abb
Christopher Eckert <christopher.k.eckert@gmail.com>
2024-09-27 09:20:40 -0700

    clean up HomeSummary component

--- pages/homes/[id].vue
+++ pages/homes/[id].vue
@@ -1,35 +1,25 @@
-<template>
-    <div v-if="home">
-      <h1>{{ home.address.street1 }}</h1>
-      <p>{{ home.villaFactScore }}</p>
-      <UTabs :items="items" />
-    </div>
-    <div v-else>
-        <p>Home not found ...</p>
-      </div>
-  </template>
-  
-  <script setup>
-  import { useRoute } from 'vue-router';
-  import { useAsyncData } from 'nuxt/app';
-  import { doc, getDoc } from 'firebase/firestore';
-  
+<script setup>
+
+import { useRoute } from 'vue-router';
+import { useAsyncData } from 'nuxt/app';
+import { doc, getDoc } from 'firebase/firestore';
+
 import { useCollection } from 'vuefire'
 import { collection, query, where } from 'firebase/firestore'
 
 const items = [{
-  label: 'Tab1',
-  icon: 'i-heroicons-information-circle',
-  content: 'This is the content shown for Tab1'
+label: 'Tab1',
+icon: 'i-heroicons-information-circle',
+content: 'This is the content shown for Tab1'
 }, {
-  label: 'Tab2',
-  icon: 'i-heroicons-arrow-down-tray',
-  disabled: true,
-  content: 'And, this is the content for Tab2'
+label: 'Tab2',
+icon: 'i-heroicons-arrow-down-tray',
+disabled: true,
+content: 'And, this is the content for Tab2'
 }, {
-  label: 'Tab3',
-  icon: 'i-heroicons-eye-dropper',
-  content: 'Finally, this is the content for Tab3'
+label: 'Tab3',
+icon: 'i-heroicons-eye-dropper',
+content: 'Finally, this is the content for Tab3'
 }]
 
 const route = useRoute();
@@ -38,4 +28,44 @@ const { $db } = useNuxtApp();
 const docRef = doc($db, 'properties', homeId);
 const home = useDocument(docRef)
 
-  </script>
+</script>
+
+<template>
+  <div v-if="home" class="space-y-4">
+  <HomeSummary :homeId="homeId" />
+  <HomeValue :homeId="homeId"/>
+  <article class="p-4 bg-white shadow-md rounded-md">
+      <NuxtLink to="/score">
+        <h2 class="text-lg font-bold">Level Up
+          <UIcon name="i-heroicons-chevron-double-right" class="w-4 h-4" />
+        </h2>
+        <p class="text-gray-700">Increase your home's VillaFact Score to increase your net asset value. Complete these
+          simple
+          activities to build your profile.</p>
+      </NuxtLink>
+    </article>
+    <article class="p-4 bg-white shadow-md rounded-md">
+      <NuxtLink to="/invest">
+        <h2 class="text-lg font-bold">Invest
+          <UIcon name="i-heroicons-chevron-double-right" class="w-4 h-4" />
+        </h2>
+        <p class="text-gray-700">Plan and track home improvement projects to maximize your costs basis.</p>
+      </NuxtLink>
+    </article>
+    <article class="p-4 bg-white shadow-md rounded-md">
+      <NuxtLink to="/advice">
+        <h2 class="text-lg font-bold">Advice
+          <UIcon name="i-heroicons-chevron-double-right" class="w-4 h-4" />
+        </h2>
+        <p class="text-gray-700">GenAI Powered advice for homeowners about the best way to increase the value and lower
+          the costs of your home.</p>
+      </NuxtLink>
+    </article>
+  </div>
+  <div v-else class="space-y-4">
+    <article class="p-4 bg-white shadow-md rounded-md">
+      <p>Home not found ...</p>
+    </article>
+  </div>
+</template>
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,52 +1,12 @@
 <script setup>
-import { onMounted, ref } from 'vue'
-
-//import { onMounted } from "vue"
-import { getDoc, doc } from 'firebase/firestore'
-
-const { $db } = useNuxtApp();
+// const homeId = route.params.id;
 const homeId = "FK9dzfK8PK7QRazTHn5f"
-const home = ref()
-const docRef = doc($db, "properties", homeId);
-const docSnap = await getDoc(docRef);
-
-onMounted(() => {
-  if (docSnap.exists()) {
-    home.value = docSnap.data()
-  } else {
-    // docSnap.data() will be undefined in this case
-    console.log("No such document!");
-  }
-})
-
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
-import { Bar } from 'vue-chartjs'
-import GaugeChart from '~/components/GaugeChart.vue'
-import LineChart from '~/components/LineChart.vue'
-
-// Register
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
-
-const chartData = ref({
-  labels: ['January', 'February', 'March', 'April', 'May'],
-  datasets: [
-    {
-      label: 'Net Home Value',
-      backgroundColor: '#f87979',
-      data: [400000, 401000, 402000, 400500, 450000],
-    },
-  ],
-})
-const chartOptions = ref({
-  responsive: true,
-  maintainAspectRatio: true,
-})
 
 </script>
 <template>
   <div class="space-y-4">
-  <HomeSummary />
-  <HomeValue />
+  <HomeSummary :homeId="homeId" />
+  <HomeValue :homeId="homeId" />
   <article class="p-4 bg-white shadow-md rounded-md">
       <NuxtLink to="/score">
         <h2 class="text-lg font-bold">Level Up
