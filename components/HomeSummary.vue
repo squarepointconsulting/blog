b43d3780b5e8f1f82a67d98b6edb59cfde736d19
Christopher Eckert <christopher.k.eckert@gmail.com>
2024-10-01 13:39:37 -0700

    refactor

--- components/HomeSummary.vue
+++ components/HomeSummary.vue
@@ -88,7 +88,7 @@ onMounted(() => {
                 </div>
                 <!-- Row 2 -->
                 <div class="w-full flex justify-center items-center">
-                    <p class="font-bold text-center">{{ homeSource.villafactScore }}</p>
+                    <p class="font-bold text-center">{{ homeSource.villaFactScore ? homeSource.villaFactScore : homeSource.villafactScore}}</p>
                 </div>
             </div>
             <!-- Edit Button -->
--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,18 @@
 <script setup>
 import { onMounted, ref } from 'vue'
-import { useCollection } from 'vuefire'
 import { collection, query, where, getDocs } from 'firebase/firestore'
 
-
-
-// const homeId = route.params.id;
-// find the home id for the "default" home for this user.
-// for now, that is just the first home for this user. 
 const homeId = ref("")
-//const homeId = ref("FK9dzfK8PK7QRazTHn5f")
 const user = await getCurrentUser()
-console.log(`user.uid: ${user.uid}`)
-
 const { $db } = useNuxtApp();
 const q = query(collection($db, "properties"), where("ownerId", "==", user.uid));
 const querySnapshot = await getDocs(q);
-console.log(querySnapshot.empty)
-
-
-
-// homeId.value = getDocs(q).then((querySnapshot) => {
-//   console.log(querySnapshot)
-//   if (!querySnapshot.empty) {
-//     return querySnapshot[0].id
-//   }
-//  })
-
-
-//homeId.value = homes.first.id
-//console.log(`homeId.value: ${ homeId.value}`)
 
 onMounted(() => {
-    querySnapshot.forEach((doc) => {
-      homeId.value = doc.id;
-      console.log(doc.id);    
-    })
+  // TODO: This is dumb. Allow user to choose "default" home.   
+  querySnapshot.forEach((doc) => {
+    homeId.value = doc.id;
+  })
     
 })
 
